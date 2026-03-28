651edb26672cb73001f69a188d7d2c77dbf28397
Luan Santos <luansantostoto@gmail.com>
2026-03-28 14:39:49 -0300

    Update GetPid.cpp

--- UnhookTeste.cpp
+++ UnhookTeste.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <Windows.h>
-#include <vector>
 #include <TlHelp32.h>
 
 DWORD GetPid(const wchar_t* procName)
@@ -26,52 +25,7 @@ DWORD GetPid(const wchar_t* procName)
     return pid;
 }
 
-
-/*DWORD GetPid(const char* processName)
-{
-    std::wstring wchar = PCharToWString(processName);
-    const wchar_t* wProcessName = wchar.data();
-
-    DWORD pid = 0;
-
-    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (!hSnapshot) return 0;
-
-    PROCESSENTRY32 pe;
-    pe.dwSize = sizeof(PROCESSENTRY32);
-
-    if (!Process32First(hSnapshot, &pe)) {
-        CloseHandle(hSnapshot);
-        return 0;
-    }
-
-    do {
-        if (_wcsicmp(pe.szExeFile, wProcessName) != 0) continue;
-        pid = pe.th32ProcessID;
-        break;
-    } while (Process32Next(hSnapshot, &pe));
-
-    CloseHandle(hSnapshot);
-    return pid;
-}*/
-
-bool Unhooked(HANDLE hProcess, const char* modeuleName, const char* APIname, const DWORD numBytes)
-{
-	FARPROC hLoadA = GetProcAddress(GetModuleHandleA(modeuleName), APIname);
-	if (!hLoadA) return false;
-
-	std::vector<BYTE> arr(numBytes);
-	LPVOID pArr = arr.data();
-
-	MoveMemory(pArr, hLoadA, numBytes);
-
-	SIZE_T numBytesWriten;
-	return WriteProcessMemory(hProcess, hLoadA, pArr, numBytes, &numBytesWriten);
-}
-
 int main()
 {
-	
-
 }
 
